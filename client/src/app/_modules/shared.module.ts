7eb9bb52330d2510d3edb5e9abe015558a639d71
R4m21 <cr4m21@gmail.com>
2021-08-08 13:33:28 +0530

    End of section 6

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    FontAwesomeModule,
+    ToastrModule.forRoot({
+      positionClass:'toast-bottom-right'
+    })
+  ],
+  exports:[
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
